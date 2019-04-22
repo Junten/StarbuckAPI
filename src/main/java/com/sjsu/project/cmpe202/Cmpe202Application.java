104377dced5c1c054b10e7a4285743ee14fa889e
junteng <junteng361@gmail.com>
2019-04-22 16:06:07 -0700

    init commit

---
+++ src/main/java/com/sjsu/project/cmpe202/Cmpe202Application.java
@@ -0,0 +1,13 @@
+package com.sjsu.project.cmpe202;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cmpe202Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cmpe202Application.class, args);
+	}
+
+}
